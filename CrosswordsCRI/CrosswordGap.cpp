cf1074746e0eb952aefc0e853b926dc853b8f340
UnRusoEnBolas <jgimenezp99@gmail.com>
2020-10-05 16:41:41 +0200

    .

--- CrosswordsCRI/CrosswordGap.cpp
+++ CrosswordsCRI/CrosswordGap.cpp
@@ -4,3 +4,35 @@ CrosswordGap::CrosswordGap(list<CrosswordDeckCell*> cells, Direction direction) 
 	this->cells = cells;
 	this->direction = direction;
 }
+
+bool CrosswordGap::setWord(string word) {
+	if (word.size() != this->cells.size()) return false;
+
+	int idx = 0;
+	for (list<CrosswordDeckCell*>::iterator it = this->cells.begin(); it != this->cells.end(); it++) {
+		char actualLetterInCell = (*it)->getLetter();
+		if (actualLetterInCell != '-' && actualLetterInCell != word[idx]) return false;
+		idx++;
+	}
+	
+	idx = 0;
+	for (list<CrosswordDeckCell*>::iterator it = this->cells.begin(); it != this->cells.end(); it++) {
+		(*it)->setLetter(word[idx]);
+		idx++;
+	}
+	return true;
+}
+
+string CrosswordGap::getWord() {
+	string word;
+	for (list<CrosswordDeckCell*>::iterator it = this->cells.begin(); it != this->cells.end(); it++) {
+		word.push_back((*it)->getLetter());
+	}
+	return word;
+}
+
+void CrosswordGap::removeWord() {
+	for (list<CrosswordDeckCell*>::iterator it = this->cells.begin(); it != this->cells.end(); it++) {
+		(*it)->removeLetter();
+	}
+}
