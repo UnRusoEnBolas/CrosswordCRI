99a3570d220060ca2e129ad5e92f0b5304756263
Carlos <ctarrias11@gmail.com>
2020-10-05 14:57:17 +0200

    Definición i implementación del diccionario modificadas

--- CrosswordsCRI/CrosswordDictionary.h
+++ CrosswordsCRI/CrosswordDictionary.h
@@ -1,10 +1,14 @@
 #include <unordered_map>
+#include <list>
+#include <vector>
 #include <iostream>
+
 
 /**
 * CLASS CrosswordDictionary
 * Classe que permet guardar les paraules del crossword i la seva longitud en un diccionari
-* Conté una estructura unordered_map per emmagatzemar les paraules i la seva longitud
+* Conté una estructura unordered_map per emmagatzemar les paraules en una llista segons la seva longitud
+* Conte un array de llistes per poder emmagtzemar paraules de una longitud a la seva respectiva llista
 * Conte metodes per inicialitzar el diccionari amb un fitxer i per printar-lo per pantalla
 * 
 */
@@ -13,10 +17,14 @@
 class CrosswordDictionary
 {
 public:
-	CrosswordDictionary() {}
+	CrosswordDictionary() { m_llistes.resize(MAX_LLISTES); }
 	~CrosswordDictionary();
 	void setDictionary(std::string file_name);
     void printDictionary();
+	void emplaceWordtoList(std::string& word);
+	
 private:
-	std::unordered_map<std::string, int> m_dictionary;
+	static const int MAX_LLISTES = 15;
+	std::unordered_map<int, std::list<std::string>> m_dictionary;
+	std::vector<std::list<std::string>> m_llistes; //tamany maxim 15
 };
